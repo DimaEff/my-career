55a06af6fbb18cfee7a2a98d9562f4b2ffc93d82
Dima Fominenkov <69797190+DimaEff@users.noreply.github.com>
2025-01-22 01:52:39 +0300

    init monorepo, elysia, react, connect with trpc

---
+++ packages/server/src/index.ts
@@ -0,0 +1,20 @@
+import cors from "@elysiajs/cors";
+import { compile as c, trpc } from "@elysiajs/trpc";
+import { initTRPC } from "@trpc/server";
+import { Elysia, t as T } from "elysia";
+import { z } from "zod";
+
+const t = initTRPC.create();
+
+const trpcAppRouter = t.router({
+  ping: t.procedure
+    .input(z.object({
+      name: z.string(),
+      meta: z.string().optional(),
+    }))
+    .query(({ input }) => `pong ${input.name} ${input.meta ?? ''}`),
+});
+
+export type TrpcRouter = typeof trpcAppRouter;
+
+const app = new Elysia().use(cors()).use(trpc(trpcAppRouter, { endpoint: '/trpc' })).listen(3000, () => console.log("Elysia has started on port 3000!"));
